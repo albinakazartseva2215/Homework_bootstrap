83e49b3df9a28ea5e721e196d219248654863acb
albina_kazartseva <albinak74@gmail.com>
2025-04-27 19:05:49 +0500

    homework_21_2

--- src/main.py
+++ src/main.py
@@ -1,29 +1,29 @@
 # Импорт встроенной библиотеки для работы веб-сервера
-import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from pathlib import Path
 
 # Для начала определим настройки запуска
 hostName = "localhost" # Адрес для доступа по сети
-serverPort = 8000 # Порт для доступа по сети
+serverPort = 8080 # Порт для доступа по сети
 
 class MyServer(BaseHTTPRequestHandler):
     """
         Специальный класс, который отвечает за
         обработку входящих запросов от клиентов
     """
-    def do_get(self):
+    def do_GET(self):
         """ Метод для обработки входящих GET-запросов """
         try:
-            self.send_response(200) # Отправка кода ответа
-            self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
-            self.end_headers() # Завершение формирования заголовков ответа
-            file_path = os.path.join(
-                os.path.dirname(__file__),
-                "bootstrap-5.3.5-dist",
-                "contacts.html"
+            file_path = (
+                    Path(__file__).parent.parent
+                    / "bootstrap-5.3.5-dist"
+                    / "contacts.html"
             )
             with open(file_path, "r", encoding="utf-8") as file:
                 data = file.read()
+            self.send_response(200) # Отправка кода ответа
+            self.send_header("Content-type", "text/html; charset=utf-8") # Отправка типа данных, который будет передаваться
+            self.end_headers() # Завершение формирования заголовков ответа
             self.wfile.write(bytes(data, "utf-8"))
 
         except FileNotFoundError:
@@ -33,6 +33,18 @@ class MyServer(BaseHTTPRequestHandler):
             self.send_error(500, f"Server Error: {str(e)}")
 
 if __name__ == "__main__":
+    # найдем свободный порт
+    # import socket
+    #
+    #
+    # def find_free_port():
+    #     with socket.socket() as s:
+    #         s.bind(('', 0))
+    #         return s.getsockname()[1]
+    #
+    #
+    # serverPort = find_free_port()
+
     webServer = None
     try:
         webServer = HTTPServer((hostName, serverPort), MyServer)
